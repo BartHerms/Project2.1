b01bf2bbf3eaded1621abc01137980fa1996d39e
Giovanni de Vries <giovanni.de.vries@student.nhlstenden.com>
2022-02-28 16:00:42 +0100

    change the whole folder structure combined multiple files

--- public/footer.php
+++ public/footer.php
@@ -1,18 +1,15 @@
-<?php include_once 'db_products.php' ?>
-<?php include_once 'header.php' ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NHL Webshop</title>
+    <title>Document</title>
 </head>
-
 <body>
-    <h1>Hey ! welcome to main page .</h1>
+<footer>
+    <p>© 2022 NHL Stenden</p>
+</footer>
+</div>
 </body>
-
 </html>
