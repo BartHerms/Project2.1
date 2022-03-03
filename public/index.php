07151950365e082ec2ba607400084953460ba3af
JonathanHeezen <jonathan.heezen@student.nhlstenden.com>
2022-03-03 16:01:37 +0100

    bijna correcte navbar met bijna correcte filter op de homepage

--- public/index.php
+++ public/index.php
@@ -16,9 +16,60 @@ if(!isset($_SESSION["userEmail"])) ?>
 </head>
 
 <body>
-    <?php include_once 'header.php' ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="product-sort">
+            <h1> Found products </h1>
+                <label for="sort">Sorteer producten</label>
+                <select name="sort" id="sort">
+                    <option value="all" <?php echo (!isset($_POST['sort']) == 'all') ? "selected" : ""; ?>>Alle Producten</option>
+                    <option value="Nop" <?php echo (isset($_POST['sort']) == 'Nop') ? "selected" : ""; ?>>Naam Oplopend</option>
+                    <option value="Naf" <?php echo (isset($_POST['sort']) == 'Naf') ? "selected" : ""; ?>>Naam Aflopend</option>
+                    <option value="Pop" <?php echo (isset($_POST['sort']) == 'Pop') ? "selected" : ""; ?>>Prijs Oplopend</option>
+                    <option value="Paf" <?php echo (isset($_POST['sort']) == 'Paf') ? "selected" : ""; ?>>Prijs Aflopend</option>
+                </select>
+                <input type="submit" style='padding:0px; background-color: none; border:0px;' value="&#9989" name="submit">
+            </form>
 
-    <h1> Found Products </h1>
+            <?php 
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST"){ 
+                if(!empty($_POST['sort'])){
+
+                    if(!empty($_POST['sort']) == 'all'){
+                    echo"Alle producten ";
+                    }
+                    else{echo "test";}
+
+                    if(!empty($_POST['sort']) == 'Nop'){
+                        echo"alfabetisch-Oplopende volgorde ";
+                    }
+                    else{echo "";}
+                    
+                    if(!empty($_POST['sort']) == 'Naf'){
+                        echo"alfabetisch-Aflopende volgorde ";
+                    }
+                    else {echo "";} 
+
+                    if(!empty($_POST['sort']) == 'Pop'){
+                        echo"Prijs-Oplopende volgorde ";
+                    }
+                    else {echo "";} 
+
+                    if(!empty($_POST['sort']) == 'Paf'){
+                        echo"Prijs-aflopende volgorde ";
+                    }
+                    else {echo "";} 
+                }
+                else
+                { 
+                echo "ALLE";
+                } 
+            }
+            else {echo "No post done yet";}
+            ?>
+        </div>
+        <?php /*if (mysqli_stmt_num_rows($statement) > 0): */?>
+    </div>
+
 
     <?php include_once 'footer.php' ?>
 </body>
