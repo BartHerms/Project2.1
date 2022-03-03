c183eea9f47982c04022e8e046a786671afaa797
BartHerms <91067528+BartHerms@users.noreply.github.com>
2022-03-03 14:48:34 +0100

    Product page
    begin product page gemaakt

--- public/index.php
+++ public/index.php
@@ -21,6 +21,33 @@ if(!isset($_SESSION["userEmail"])) ?>
     <h1> Found Products </h1>
 
     <?php include_once 'footer.php' ?>
+    <?php fetchValues($conn); ?>
 </body>
 
 </html>
+
+<?php
+function fetchValues($conn)
+{
+    $sql = "SELECT title, description, category, price FROM products";
+    $stmt = mysqli_prepare($conn, $sql) or die(mysqli_error($conn));
+    mysqli_stmt_execute($stmt) or die(mysqli_error($conn));
+    mysqli_stmt_bind_result($stmt, $prodName, $prodDescription, $prodCategory, $prodPrice);
+    mysqli_stmt_store_result($stmt);
+    if (mysqli_stmt_num_rows($stmt) > 0) {
+        while (mysqli_stmt_fetch($stmt)) {
+           
+            echo '
+            <tr>
+            <th>' . $prodName . '</th>
+            <th>' . $prodDescription . '</th>
+            <th>' . $prodCategory . '</th>
+            <th>' . $prodPrice . '</th>
+            <br> </br>
+            </tr>';
+        }
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+}
+?>
